12d22a940b7d0763f311453b0d9e8e7ea1027c78
Anatole Debierre <anatole.debierre@gmail.com>
2020-12-24 16:40:26 +0100

    Remove debug code

--- lib/index.ts
+++ lib/index.ts
@@ -3,9 +3,3 @@ import { Session } from "./Session";
 import * as types from "./types";
 
 export { Session, types, accounts };
-
-(async () => {
-	const session = new Session("EDPERSONNEL", "0");
-	const account = await session.login();
-	console.log(account.type);
-})();
