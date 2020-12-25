04f2b95d2e7461a0e4d49cd23ed994fc2d2f2780
Anatole <62328077+a2br@users.noreply.github.com>
2020-12-25 14:29:53 +0100

    Add Jest tests

---
+++ __tests__/isFailure.test.ts
@@ -0,0 +1,50 @@
+import * as fs from "fs";
+import * as path from "path";
+
+import { isFailure } from "../lib/functions";
+
+import { _loginRes } from "../lib/types";
+
+const getDirectories = (srcPath: string) =>
+	fs
+		.readdirSync(srcPath)
+		.filter((file) => fs.statSync(path.join(srcPath, file)).isDirectory());
+
+const readJSONFile = (path: string) =>
+	JSON.parse(fs.readFileSync(path).toString());
+
+const responses = getDirectories("__tests__/responses").map((dirName) => {
+	const path = `__tests__/responses/${dirName}`;
+
+	const map: {
+		name: string;
+		success: _loginRes[];
+		failure: _loginRes[];
+	} = {
+		name: dirName,
+		success: readJSONFile(`${path}/success.json`),
+		failure: readJSONFile(`${path}/failure.json`),
+	};
+
+	return map;
+});
+
+responses.forEach((resType) => {
+	test(resType.name, () => {
+		resType.success.forEach((res) => expect(isFailure(res)).toBe(false));
+		resType.failure.forEach((res) => expect(isFailure(res)).toBe(true));
+	});
+});
+
+// test("login", () => {
+// 	//! SUCCESS
+// 	successLogin.forEach((res) => {
+// 		const judgement = isFailure(res);
+// 		expect(judgement).toBe(false);
+// 	});
+// 	//! FAILURE
+// 	failureLogin.forEach((res) => {
+// 		const judgement = isFailure(res);
+// 		expect(judgement).toBe(true);
+// 	});
+// });
---
+++ __tests__/session.test.ts
@@ -0,0 +1,26 @@
+import { Session } from "../lib/index";
+
+const session = new Session("EDELEVE", "0");
+const account = session.login();
+
+test("credentials", () => {
+	expect(JSON.stringify(session.credentials)).toBe(
+		JSON.stringify({
+			username: "EDELEVE",
+			password: "0",
+		})
+	);
+});
+
+test("account", async () => {
+	const acc = await account;
+	expect(acc.type).toBe("student");
+});
+
+test("homework", async () => {
+	const acc = await account;
+
+	if (acc.type !== "student") return;
+	const someHomework = await acc.getHomework("2021-01-14");
+	expect(Array.isArray(someHomework)).toBe(true);
+});
