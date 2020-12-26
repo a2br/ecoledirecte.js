aa4aa317c0a9ef4ee5b5ead7b9f8a2e7594b867e
Anatole <62328077+a2br@users.noreply.github.com>
2020-12-26 16:15:22 +0100

    Clean getMessages + init cleanMessages

--- lib/functions/student/mailbox.ts
+++ lib/functions/student/mailbox.ts
@@ -1,26 +1,21 @@
-import fetch from "node-fetch";
+import { makeRequest } from "../util";
 
 import { _mailboxRes } from "../../types";
+import { _mailboxResSuccess } from "../../types/student/mailbox";
 
 export async function getMessages(
 	id: number,
 	token: string,
 	direction: "received" | "sent" = "received"
 ) {
-	let urlencoded = new URLSearchParams();
-	urlencoded.append(
-		"data",
-		JSON.stringify({
-			token: token,
-		})
-	);
-	let edRes = await fetch(
-		`https://api.ecoledirecte.com/v3/eleves/${id}/messages.awp?verbe=getall&typeRecuperation=${direction}&orderBy=date&order=desc&page=0&itemsPerPage=100&onlyRead=&query=&idClasseur=0`,
-		{
-			method: "POST",
-			body: urlencoded,
-		}
-	);
-	let body: _mailboxRes = await edRes.json();
+	const body: _mailboxResSuccess = await makeRequest({
+		method: "POST",
+		url: `https://api.ecoledirecte.com/v3/eleves/${id}/messages.awp?verbe=getall&typeRecuperation=${direction}&orderBy=date&order=desc&page=0&itemsPerPage=100&onlyRead=&query=&idClasseur=0`,
+		body: { token },
+		guard: true,
+	});
+
 	return body;
 }
+
+export function cleanMessages(mailboxRes: _mailboxResSuccess, token: string) {}
