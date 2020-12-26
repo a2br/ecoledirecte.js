29b90812c1f48b946888c5e62b1bef3fb9b0f22a
Anatole <62328077+a2br@users.noreply.github.com>
2020-12-26 18:19:56 +0100

    Remove debug

--- lib/index.ts
+++ lib/index.ts
@@ -3,11 +3,3 @@ import { Session } from "./Session";
 
 export { Session, accounts };
 export default Session;
-(async () => {
-	const s = new Session("EDELEVE", "0");
-	const a = await s.login().catch(console.log);
-	if (!a || a.type !== "student") return;
-
-	const messages = await a.getMessages();
-	console.log(messages[0]);
-})();
