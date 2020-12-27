5d864a3142a48bc235584f0cc7ffaaecfce31522
Anatole <62328077+a2br@users.noreply.github.com>
2020-12-27 22:08:03 +0100

    More JSDocs

--- lib/types/student/mailbox.ts
+++ lib/types/student/mailbox.ts
@@ -90,15 +90,31 @@ type _mailboxResMessage = {
 };
 
 export type message = {
+	/** @description Unique ID */
 	id: number;
+	/** @description Direction of the message */
 	type: "received" | "sent";
+	/** @description Whether it is marked as read */
 	read: boolean;
+	/** @description Has it been transferred? */
 	transferred: boolean;
+	/** @description Has it been answered? */
 	answered: boolean;
-	to_cc_cci: "to" | "cc" | "cci";
+	/**
+	 * @description Degree of implication
+	 * @enum `to`: the message is directed to the user
+	 * @enum `cc`: the message is not written for the user, but publicly adressed
+	 * @enum `cci`: the message is not written for the user and is privately sent
+	 * @enum ` `: the message isn't received
+	 */
+	to_cc_cci: "to" | "cc" | "cci" | "";
+	/** @description Subject, headline of the mail */
 	subject: string;
+	/** @description Fetches the content of the mail */
 	getContent: () => Promise<expandedBase64>;
+	/** @description When has this message been sent? */
 	date: Date;
+	/** @description List of the addressees */
 	to: Array<{
 		id: number;
 		fullName: string;
@@ -110,6 +126,7 @@ export type message = {
 		read: boolean;
 		to_cc_cci: "to" | "cc" | "cci";
 	}>;
+	/** @description Sender */
 	from: {
 		id: number;
 		fullName: string;
@@ -120,6 +137,7 @@ export type message = {
 		role: role;
 		read: boolean;
 	};
+	/** @description Raw document straight from EcoleDirecte */
 	_raw: _mailboxResMessage;
 };
 
--- lib/types/student/textbook.ts
+++ lib/types/student/textbook.ts
@@ -2,31 +2,41 @@ import { expandedBase64 } from "..";
 import { _failureRes } from "../failureRes";
 
 export type assignement = {
+	/** @description Unique ID */
 	id: number;
+	/** @description Date for which the assignement is due */
 	date: Date;
+	/** @description Whether it represents a test or not */
 	test: boolean;
+	/** @description Concerned subject */
 	subject: {
 		name: string;
 		code: string;
 	};
+	/** @description Teacher who created the assignement */
 	teacher: string;
+	/** @description The homework itself */
 	job?: {
 		content: expandedBase64;
 		givenAt: Date;
+		/** @description Whether the homework should be returned through EcoleDirecte */
 		toReturnOnline: boolean;
 		done: boolean;
+		/** @description `contenuDeSeance` of the last course */
 		lastContenuDeSeance: {
 			content: expandedBase64;
 			documents: unknown[];
 		};
-		/** Experimental */
-		tick: (newState: boolean) => Promise<boolean>;
+		/** @description Mark the homework as done or not. No `newState` will set it as the opposite of the current state */
+		tick: (newState?: boolean) => Promise<boolean>;
 	};
+	/** @description The day's `contenuDeSeance`. May not be displayed in the EcoleDirecte UI if the date is in the future */
 	contenuDeSeance: {
 		homeworkId: number;
 		content: expandedBase64;
 		documents: unknown[];
 	};
+	/** @description Raw document straight from EcoleDirecte */
 	_raw: _textbookDateAssignement;
 };
 
