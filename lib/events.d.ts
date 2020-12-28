b87351ddb99c9bd8bf12a1c90b49f9c8c3e7693b
Anatole Debierre <anatole.debierre@gmail.com>
2020-12-28 15:52:23 +0100

    Try to fix stuff

--- lib/events.d.ts
+++ lib/events.d.ts
@@ -12,42 +12,42 @@ declare class EventsManager extends EventEmitter {
 		event: K,
 		listener: (...args: LogEvents[K]) => void
 	): this;
-	public on<S extends string | symbol>(
-		event: Exclude<S, keyof LogEvents>,
-		listener: (...args: unknown[]) => void
-	): this;
+	// public on<S extends string | symbol>(
+	// 	event: Exclude<S, keyof LogEvents>,
+	// 	listener: (...args: unknown[]) => void
+	// ): this;
 
 	public once<K extends keyof LogEvents>(
 		event: K,
 		listener: (...args: LogEvents[K]) => void
 	): this;
-	public once<S extends string | symbol>(
-		event: Exclude<S, keyof LogEvents>,
-		listener: (...args: unknown[]) => void
-	): this;
+	// public once<S extends string | symbol>(
+	// 	event: Exclude<S, keyof LogEvents>,
+	// 	listener: (...args: unknown[]) => void
+	// ): this;
 
 	public emit<K extends keyof LogEvents>(
 		event: K,
 		...args: LogEvents[K]
 	): boolean;
-	public emit<S extends string | symbol>(
-		event: Exclude<S, keyof LogEvents>,
-		...args: unknown[]
-	): boolean;
+	// public emit<S extends string | symbol>(
+	// 	event: Exclude<S, keyof LogEvents>,
+	// 	...args: unknown[]
+	// ): boolean;
 
 	public off<K extends keyof LogEvents>(
 		event: K,
 		listener: (...args: LogEvents[K]) => void
 	): this;
-	public off<S extends string | symbol>(
-		event: Exclude<S, keyof LogEvents>,
-		listener: (...args: unknown[]) => void
-	): this;
+	// public off<S extends string | symbol>(
+	// 	event: Exclude<S, keyof LogEvents>,
+	// 	listener: (...args: unknown[]) => void
+	// ): this;
 
 	public removeAllListeners<K extends keyof LogEvents>(event?: K): this;
-	public removeAllListeners<S extends string | symbol>(
-		event?: Exclude<S, keyof LogEvents>
-	): this;
+	// public removeAllListeners<S extends string | symbol>(
+	// 	event?: Exclude<S, keyof LogEvents>
+	// ): this;
 }
 declare const logs: EventsManager;
 export default logs;
