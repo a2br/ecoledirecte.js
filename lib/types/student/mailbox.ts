ffa9ce264bc4ec57ea91fef3ae6ac719bb271b5c
Anatole Debierre <anatole.debierre@gmail.com>
2020-12-29 15:54:12 +0100

    Fix export

--- lib/types/student/mailbox.ts
+++ lib/types/student/mailbox.ts
@@ -5,6 +5,7 @@ export {
 	mailboxRes as _mailboxRes,
 	mailboxResSuccess as _mailboxResSuccess,
 	mailboxResMessage as _mailboxResMessage,
+	messageResSuccess as _messageResSuccess,
 } from "ecoledirecte-api-types";
 
 export type message = {
