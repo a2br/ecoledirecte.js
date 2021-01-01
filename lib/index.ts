d7df7a5125e57e91a6bb37e0ab95c0f186aca249
Anatole Debierre <anatole.debierre@gmail.com>
2021-01-01 14:42:02 +0100

    EDAPIErr export

--- lib/index.ts
+++ lib/index.ts
@@ -1,7 +1,8 @@
 import { Session } from "./Session";
 import events from "./events";
+import { EcoleDirecteAPIError } from "./errors";
 
-export { Session, events };
+export { Session, events, EcoleDirecteAPIError };
 export * from "./account_types";
 
 export default Session;
