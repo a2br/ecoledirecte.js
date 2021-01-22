b96ded03e50a53f46ca3d8a6bdb4f4e39a87bc0e
Anatole <62328077+a2br@users.noreply.github.com>
2021-01-22 13:25:35 +0100

    remove debug code

--- lib/index.ts
+++ lib/index.ts
@@ -6,10 +6,3 @@ export { Session, events, EcoleDirecteAPIError };
 export * from "./account_types";
 
 export default Session;
-
-(async () => {
-	const s = new Session("adebierre-spc", "gbUi8y");
-	const a = await s.login();
-	if (a.type !== "student") return;
-	await a.getPhoto();
-})();
