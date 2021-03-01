e6e08cd7f953ea9d69bb968bb591a33dbf6e7ae8
Anatole Debierre <62328077+a2br@users.noreply.github.com>
2021-03-01 16:56:03 +0100

    Add note

--- lib/events.ts
+++ lib/events.ts
@@ -1,4 +1,7 @@
 import { EventEmitter } from "events";
 class EventManager extends EventEmitter {}
+/**
+ * Important note: the profile picture requests don't trigger the event manager
+ */
 const events = new EventManager();
 export default events;
