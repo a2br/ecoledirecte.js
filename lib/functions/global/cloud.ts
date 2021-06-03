bd99844358f1cb2bcc18e7aad17865f08d1c4199
ModernChocolate <modernchocolate@users.noreply.github.com>
2021-06-03 23:10:28 +0200

    Update headers and close #128

--- lib/functions/global/cloud.ts
+++ lib/functions/global/cloud.ts
@@ -29,15 +29,11 @@ export async function getCloudFolder(
 	return body;
 }
 
-const EdHeadersFile = {
+export const EdHeadersFile = {
 	authority: "api.ecoledirecte.com",
-	"sec-ch-ua":
-		// eslint-disable-next-line quotes
-		'"Google Chrome";v="89", "Chromium";v="89", ";Not A Brand";v="99"',
 	accept: "application/json, text/plain, */*",
-	"sec-ch-ua-mobile": "?0",
 	"user-agent":
-		"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36",
+		"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36",
 	"content-type": "application/x-www-form-urlencoded",
 	origin: "https://www.ecoledirecte.com",
 	"sec-fetch-site": "same-site",
