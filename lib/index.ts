531978874138e4c8851c7c5dc056d6887dfa1982
Anatole Debierre <62328077+a2br@users.noreply.github.com>
2021-08-08 18:53:14 +0200

    Export Config things

--- lib/index.ts
+++ lib/index.ts
@@ -5,5 +5,6 @@ import { EcoleDirecteAPIError } from "./errors";
 export { Session, events, EcoleDirecteAPIError };
 export * from "./accounts";
 export * from "./classes";
+export { Config, FullConfig, PartialConfig, DefaultConfig } from "./util/util";
 
 export default Session;
